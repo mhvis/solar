94e03272c0f79dbe23838543d7c2cb41caa9ff40
Maarten Visscher <mail@maartenvisscher.nl>
2016-05-17 01:22:30 +0200

    Small update

--- solar_uploader.py
+++ solar_uploader.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3.5
+#
+# solar_uploader.py
+#
+# Daemon for automatically uploading SamilPower data to PVOutput. Uses solar.py
+# and pvoutput.py
+#
 
 import sched
+import solar
+import pvoutput
